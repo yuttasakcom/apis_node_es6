776facc1a9bd51ea9db77eb8fc28e05c8bd0b7a0
Yuttasak Pannawat <yuttasak@hot-now.com>
2017-06-22 15:55:49 +0700

    Set server run protocal https port 8443

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import express from 'express'
+import https from 'https'
 import router from './routes/router'
+import fs from 'fs'
+import path from 'path'
 
 const app = express()
 router(app)
 
-const port = process.env.PORT || 3000
-app.listen(port, () => {
+const env = process.env.NODE_ENV || 'prod'
+const options = {
+  key: fs.readFileSync(`${__dirname}/config/${env}/default.key`, 'utf8'),
+  cert: fs.readFileSync(`${__dirname}/config/${env}/default.pem`, 'utf8')
+}
+
+const server = https.createServer(options, app)
+
+const port = process.env.PORT || 8443
+
+server.listen(port, () => {
     console.log(`Server started on port ${port}`)
 })
