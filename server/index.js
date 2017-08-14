9487ee1a5e3794436f76453a97bdcbeb1c377d2c
YoProgrammer <yuttasakcom@gmail.com>
2017-08-14 23:09:24 +0700

    Set routes client on index.js server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
 
-app.use('/', (req, res) => {
+app.use('/api', (req, res) => {
   res.send({msg: 'OK'})
 })
+
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path')
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
