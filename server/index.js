04c70ec04e0198cd77ab94572bae2fafffb0621b
YoProgrammer <yuttasakcom@gmail.com>
2017-08-14 23:56:10 +0700

    Add mongoose & google-oauth flow

--- server/index.js
+++ server/index.js
@@ -1,9 +1,28 @@
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+const bodyParser = require('body-parser')
+const keys = require('./config/keys')
 
-app.use('/api', (req, res) => {
-  res.send({msg: 'OK'})
-})
+require('./models/User')
+require('./services/passport')
+
+mongoose.Promise = global.Promise
+mongoose.connect(keys.mongoURL)
+
+const app = express()
+app.use(bodyParser.json())
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+
+require('./routes/authRoutes')(app)
 
 if (process.env.NODE_ENV === 'production') {
   const path = require('path')
